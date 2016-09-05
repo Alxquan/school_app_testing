f3c8255629a59d5a81c5c680af4894b7d9869972
Alex Quan <alxquan@gmail.com>
2016-09-05 13:23:39 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'schools/index'
+
+  get 'schools/new'
+
+  get 'schools/edit'
+
+  get 'schools/show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ spec/models/school_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe School, type :name do
+  describe 'classroom' do
+  it 'has a classroom name' do
+name = 'Science'
+expect(class_room.name).name to eq('Science')
+end
+end
+end
